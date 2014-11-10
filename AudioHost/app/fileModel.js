1a5421256bae0c5091a34e497383e4a838709b24
ByteBlast <byteblast@gmail.com>
2014-11-10 21:14:20 +0000

    Asynchronous file upload using Angular.

---
+++ AudioHost/app/fileModel.js
@@ -0,0 +1,12 @@
+﻿app.directive('fileModel', function($parse) {
+    return {
+        restrict: 'A',
+        link: function (scope, element, attributes) {
+            element.bind('change', function () {
+                var model = $parse(attributes.fileModel);
+                model.assign(scope, element[0].files[0]);
+                scope.$apply();
+            });
+        }
+    };
+});
---
+++ AudioHost/app/mainController.js
@@ -0,0 +1,19 @@
+﻿app.controller('mainController', function($scope, $http) {
+    $scope.upload = function() {
+        $scope.uploadStatus = "Uploading..";
+        var data = new FormData();
+        data.append('file', $scope.file);
+
+        $http.post('api/upload', data,
+        {
+            transformRequest: angular.identity,
+            headers: { 'Content-Type': undefined }
+        })
+        .success(function() {
+            $scope.uploadStatus = "Upload complete.";
+        })
+        .error(function() {
+            $scope.uploadStatus = "Failed to upload content.";
+        });
+    };
+});
