8866d839e66b349cd2d89d7b8d50d8afa6e89c61
ByteBlast <byteblast@gmail.com>
2014-11-12 21:36:41 +0000

    Encapsulated sound upload code in a service.

--- AudioHost/app/mainController.js
+++ AudioHost/app/mainController.js
@@ -1,14 +1,13 @@
-﻿app.controller('mainController', function ($scope) {
-
-    var fileSizeLimit = 20000000;
+﻿app.controller('mainController', function ($scope, fileUpload) {
 
     $scope.upload = function () {
 
-        if (!$scope.file) {
+        if ($scope.file == false) {
             $scope.uploadStatus = "Please choose a file to upload.";
             return;
         }
 
+        var fileSizeLimit = 20000000;
         var fileSize = Math.round(parseInt($scope.file.size));
         if (fileSize > fileSizeLimit) {
             $scope.uploadStatus = "File is too big. Maximim file size: " + fileSizeLimit;
@@ -16,27 +15,25 @@
         }
 
         $scope.uploadStatus = "Uploading..";
-
-        var postData = new FormData();
-        postData.append('file', $scope.file);
-
-        var request = new XMLHttpRequest();
-        request.upload.onprogress = function (e) {
-            var progressCompleted = Math.round(e.loaded / e.total * 100);
-            $scope.progress = progressCompleted;
-            $scope.$apply();
-        };
-        request.onreadystatechange = function () {
-            if (request.readyState == 4) {
-                if (request.status == 200) {
-                    $scope.uploadStatus = "Upload complete.";
-                } else {
-                    $scope.uploadStatus = "Upload failed.";
-                }
-                $scope.$apply();
-            }
-        };
-        request.open('POST', 'api/upload');
-        request.send(postData);
+        fileUpload.uploadFileToUrl(
+            $scope.file,
+            'api/upload',
+            onUploadCompleted,
+            onProgressChanged
+        );
     };
+
+    function onProgressChanged(progress) {
+        $scope.progress = progress;
+        $scope.$apply();
+    };
+
+    function onUploadCompleted(statusCode) {
+        if (statusCode === 200) {
+            $scope.uploadStatus = "Upload complete.";
+        } else {
+            $scope.uploadStatus = "Upload failed.";
+        }
+        $scope.$apply();
+    }
 });
