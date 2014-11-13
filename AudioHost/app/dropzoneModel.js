f1b6669d1671825ed65162bd9522c5ad4b242f4b
ByteBlast <byteblast@gmail.com>
2014-11-13 07:57:01 +0000

    Added support for drag and drop.

---
+++ AudioHost/app/dropzoneModel.js
@@ -0,0 +1,21 @@
+﻿app.directive('dropzoneModel', function ($parse) {
+    return {
+        restrict: 'A',
+        link: function(scope, element, attributes) {
+            element.bind('dragover', function (event) {
+                event.stopPropagation();
+                event.preventDefault();
+                event.dataTransfer.dropEffect = 'copy';
+            });
+            element.bind('drop', function(event) {
+                event.stopPropagation();
+                event.preventDefault();
+                var files = event.dataTransfer.files;
+                var model = $parse(attributes.dropzoneModel);
+                model.assign(scope, files[0]);
+                scope.$apply();
+                element.text(files[0].name);
+            });
+        }
+    };
+});
